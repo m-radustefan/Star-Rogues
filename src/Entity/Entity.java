0aaf285b237ebe650d5a13e277f2e2ca3b95d18b
m-radustefan <radu.stefan2030@gmail.com>
2024-04-15 10:36:06 +0300

    Updated Map + camera

--- src/Entity/Entity.java
+++ src/Entity/Entity.java
@@ -4,14 +4,8 @@ import java.awt.image.BufferedImage;
 
 public class Entity {
 
-    public int x,y;
+    public int worldX,worldY;
     public int speed;
-
-//    public BufferedImage playerIdle1,playerIdle2,playerIdle3,playerIdle4;
-//    public BufferedImage playerLeft1,playerleft2,playerLeft3,playerLeft4;
-//    public BufferedImage playerRight1,playerRight2,playerRight3,playerRight4;
-//    public BufferedImage playerUp1,playerUp2,playerUp3,playerUp4;
-//    public BufferedImage playerDown1,playerDown2,playerDown3,playerDown4;
 
     public String direction;
 
--- src/Entity/Player.java
+++ src/Entity/Player.java
@@ -16,14 +16,21 @@ public class Player extends  Entity {
     GamePanel gp;
     KeyHandler keyH;
 
+    public final int screenX;
+    public final int screenY;
+
     public Player (GamePanel gp, KeyHandler keyH) {
         this.gp = gp;
         this.keyH = keyH;
+
+        screenX = gp.screenWidth/2 - (gp.tileSize/2);
+        screenY = gp.screenHeight/2 - (gp.tileSize/2);
+
         setDefaultValues();
     }
     public void setDefaultValues() {
-         x = 100;
-         y = 100;
+         worldX = 100;
+         worldY = 100;
          speed = 4;
          direction = "left";
 
@@ -34,19 +41,19 @@ public class Player extends  Entity {
 
             if(keyH.upPressed == true){
                 direction="up";
-                y -= speed;
+                worldY -= speed;
             }
             else if(keyH.downPressed == true){
                 direction="down";
-                y += speed;
+                worldY += speed;
             }
             else if (keyH.leftPressed == true) {
                 direction="left";
-                x -= speed;
+                worldX -= speed;
             }
             else if (keyH.rightPressed == true) {
                 direction="right";
-                x += speed;
+                worldX += speed;
             }
 
             spriteCounter++;
@@ -206,6 +213,6 @@ public class Player extends  Entity {
                 break;
         }
 
-        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+        g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
     }
 }
